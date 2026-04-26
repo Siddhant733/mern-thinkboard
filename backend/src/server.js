3e97a9e25ebffd8aafd584c8dfffa6ee828bcf4c
Siddhant733 <1nc20cs073titamesiddhantramesh@gmail.com>
2026-04-26 20:51:59 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use(express.json());
+
+// Custom logger middleware
+app.use((req, res, next) => {
+  console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+  next();
+});
+
+// Routes
+app.use("/api/notes", notesRoutes);
+
+// Connect DB first, then start server
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
